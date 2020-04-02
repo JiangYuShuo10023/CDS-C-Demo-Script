7a10e236e74f6e25eb978d7b101d79258406b646
Jiang Yu-Shuo (James Jiang) <52614115+JiangYuShuo10023@users.noreply.github.com>
2020-04-02 19:34:42 +0800

    C Demo Script
    Twitter:James Jiang

---
+++ 00.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#define ABOUT "We Are Makeing An Operating System Call flyOS!"
+
+#define VERSION_OF_WINDOWS 10
+
+#define I_MISS_PYTHON "I Really Miss Python Now!!!!!!"
+
+#define x "1920"
+#define y "1080"
+
+#define COPYRIGHT "Script By OxygenWorld And Fix Bugs By FatManDJ&ProgramingMonkey IN People's Republic Of China."
+
+#define name ""
+
+
+
+/* This Is How To Use A Common */
+
+
+/*
+Copyright C 2020 FatDev Studio
+In PRC
+*/
+
+int fruit;
+
+int fpoa_agreement() {
+	printf("FPOA Agreement Version 1.0 \n");
+	printf("1:Program Most Under 200 Lines For Opensource \n");
+	printf("2:Opensorce Doesn't Means You Own It,You Should Pay Attention To 'COPYRIGHT' \n");
+  	printf("3:Last,We Opensource On GitHub,Download It Means You AGREE It \n");
+}
+
+int githubAccount() {
+  printf("Github:JiangYuShuo100123 \n");
+	printf("GitHub:iOrange_OxyWor \n");
+	printf("GitHub:ProgramingMonkey \n");
+}
+
+int floatTest() {
+	float fl;
+	printf("Enter A Float Number:");
+	scanf("%f",&fl);
+	printf("You Returnd:%f \n",fl);
+}
+
+int getsTest() {
+	char str[100];
+	printf("Testingt getFunc.Input A Value:");
+	scanf("%s",&str);
+	printf("\nYou Returnd: %s \n",str);
+
+}
+int main() {
+	githubAccount();
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+  floatTest();
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+	getsTest();
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+	
+	printf("Hello World! \n");
+	printf("This Is My First C Program!!");
+	printf("%s \n",ABOUT);
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+	printf("The Windows's Version Is %d!! \n",VERSION_OF_WINDOWS);
+	printf("%s \n",I_MISS_PYTHON);
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+	printf("Your Screen Resloution Is %s x %s \n",x,y);
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+	printf("Our GonalBasicOperatingSystem Comming Soon! See www.fatdev.name \n");
+	printf("Members:FatManDJ,OxygenWorld89,ZeroTrueHobbies \n");
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+	printf("My WSL's Unity Desktop Instlling Is So Slow.. \n");
+	printf("I'm Getting Crazy!!!! \n");
+	printf("A Chinese Joke:草(一种植物) \n");
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+	fpoa_agreement();
+
+	printf(" \n");
+	printf(" \n");
+	printf("####################################### \n");
+	printf(" \n");
+	printf(" \n");
+
+
+	printf("C Basic Scirpt By FatDev \n");
+	printf("Opensource With FPOA Agreement \n");
+	printf("%s",COPYRIGHT);
+}
